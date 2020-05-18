ea7faee8ff147e112c6c7d34f25bd7cd54a8adc0
unknown <haritscoding@gmail.com>
2020-05-19 02:50:28 +0700

    move private information to dev.js

--- index.js
+++ index.js
@@ -1,17 +1,38 @@
 const express = require('express');
 const app = express();
 const mongoose = require('mongoose');
-mongoose.connect('mongodb+srv://aris:ArisElda21@cluster0-9phcz.mongodb.net/test?retryWrites=true&w=majority',
+const bodyParser = require('body-parser');
+const cookieParser = require('cookie-parser');
+
+const config = require('./config/key');
+
+const {User} = require('./model/user');
+
+mongoose.connect(config.mongoURI,
 {useNewUrlParser:true}).
               then(()=>console.log('DB connected')).
               catch(err=>console.log(error));
 
 
+app.use(bodyParser.urlencoded({extended:true}));
+app.use(bodyParser.json());
+app.use(cookieParser());
 
 app.get('/',(req,res)=>{
-    res.send("Hello world");
-});
+    res.json({"hallo dunia":"Falkon pala kont"})
+})
 
-
+app.post('/api/users/register',(req,res)=>{
+    const user = new User(req.body)
+    user.save((err,userData)=>{
+        if(err) return res.json({
+            success:false,err
+        });
+        return res.status(200).json({
+            success:true
+        })
+    });
+   
+})
 
 app.listen(5000);
